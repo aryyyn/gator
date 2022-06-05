4277cbdb68c1b86732b6bc5760ad15fdb4a076f8
beach chicken <61741148+beachchicken@users.noreply.github.com>
2022-06-05 21:14:39 +0545

    Update Lmao.tsx

--- components/Lmao.tsx
+++ components/Lmao.tsx
@@ -14,7 +14,7 @@ function Lmao() {
           <h1 className='py-4 text-gray-700'>
             Hi, I'm <span className='text-[#5651e5]'> Aryan</span>
           </h1>
-          <h1 className='py-2 text-gray-700'>An IT Stuent</h1>
+          <h1 className='py-2 text-gray-700'>An IT Student</h1>
           <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
           Currently engaging with Web Development projects and Cyber Security challenges,
       I aspire to become a full stack web developer and/or cyber security researcher!

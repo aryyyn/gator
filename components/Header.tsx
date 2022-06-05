c9a3c6c45bb93e721b16cac3d71fb462e9142b44
aryan <aryanjha635@gmail.com>
2022-06-05 23:03:30 +0545

    added logos

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ function Header() {
 
   return (
     <header className={`${isScrolled && 'bg-[#141414]'}`}>
-      <div className = "flex items-center space-x-2 md:space-x-10 ">
+      <div className = "flex items-center space-x-4 md:space-x-15 ">
 
     
       <img
@@ -33,12 +33,12 @@ function Header() {
           className="cursor-pointer object-contain"
         />
         <ul className="hidden space-x-24 md:flex">    
-            <li className="headerLink">Home</li>
-            <li className="headerLink">Profile</li>
-            <li className="headerLink">About</li>
-            <li className="headerLink">Education</li>
-            <li className="headerLink">Contact</li>
-            
+       
+            <a  href="/Home" className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900" target="_blank">Home</a>
+  <a href="/Profile" className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900" target="_blank">Profile</a>
+  <a href="/About" className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900" target="_blank" >About</a>
+  <a href="/Portfolio" className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900" target="_blank" >Portfolio</a>
+  <a href="/Contact" className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900" target="_blank" >Contact</a>
            
             
         </ul>
@@ -48,11 +48,12 @@ function Header() {
      
 
       <div className='flex items-center space-x-4 text-sm font-light cursor-pointer rounded'>
-
+      <Link href = 'Homepage'>
       <MoonIcon className=" h-6 w-6"/>
+      </Link>
       
       <MailIcon className="h-6 w-6"/>
-      <Link href = "homepage">
+      <Link href = "Homepage">
       <img
           src="https://freesvg.org/img/turtle.png"
           width={50}
--- components/Lmao.tsx
+++ components/Lmao.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 
 
 function Lmao() {
@@ -14,11 +15,52 @@ function Lmao() {
           <h1 className='py-4 text-gray-700'>
             Hi, I'm <span className='text-[#5651e5]'> Aryan</span>
           </h1>
-          <h1 className='py-2 text-gray-700'>An IT Stuent</h1>
+          <h1 className='py-2 text-gray-700'>An IT Student</h1>
           <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
           Currently engaging with Web Development projects and Cyber Security challenges,
       I aspire to become a full stack web developer and/or cyber security researcher!
           </p>
+          <nav className="flex justify-center space-x-4">
+ 
+  <Link href = "https://discord.gg/CT764uegKa">
+  <a target="_blank" >
+      <img
+          src="https://www.freepnglogos.com/uploads/discord-logo-png/discord-logo-logodownload-download-logotipos-1.png"
+          width={50}
+          height={0}
+          alt=""
+          className="cursor-pointer rounded"
+        />
+        </a>
+         </Link>
+
+         <Link href = "https://www.instagram.com/asiancreep_/">
+         <a target="_blank" >
+      <img
+          src="https://www.freepnglogos.com/uploads/instagram-logo-png-transparent-0.png"
+          width={50}
+          height={0}
+          alt=""
+          className="cursor-pointer rounded"
+        />
+         </a>
+         </Link>
+        
+
+         <Link href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+         
+      <img
+          src="https://www.logo.wine/a/logo/Tinder_(app)/Tinder_(app)-Flame-White-Dark-Background-Logo.wine.svg" 
+          
+          width={50}
+          height={0}
+          alt=""
+          className="cursor-pointer rounded"
+        />
+        
+         </Link>
+       
+</nav>
     
     </div>
     </div>
